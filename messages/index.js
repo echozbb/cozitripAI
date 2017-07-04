735a83a5f86dcfdc8c9e6cdd6b3dd44adf6e96d4
echozhou <echozbb@gmail.com>
2017-07-04 18:11:19 +1000

    test

--- messages/index.js
+++ messages/index.js
@@ -39,8 +39,8 @@ console.log("Posting to LUIS server..." + process.env.LUIS_MODEL_URL);
 bot.dialog('/', [
     function (session, args) {
         session.send('Welcome to cozitrip! You can say like this: book 2 rooms from 2017-10-20 to 2017-10-22 in Sydney.')
-        session.send("Posting to LUIS server..." + process.env.LUIS_MODEL_URL);
-        session.send("process.env.NODE_ENV="+process.env.NODE_ENV);
+        session.send("Posting to LUIS server..." + process.env['LUIS_MODEL_URL']);
+        session.send("process.env.NODE_ENV="+process.env['NODE_ENV']);
     }
 ]);
 
