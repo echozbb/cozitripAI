3e5da79e0ae0543b9e5eba1a9c6f9dc533cba510
echozhou <echozbb@gmail.com>
2017-07-04 12:15:30 +1000

    add log

--- messages/index.js
+++ messages/index.js
@@ -34,6 +34,8 @@ bot.localePath(path.join(__dirname, './locale'));
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
+console.log("Posting to LUIS server..." + process.env.LUIS_MODEL_URL);
+
 bot.dialog('/', [
     function (session, args) {
         session.send('Welcome to cozitrip! You can say like this: book 2 rooms from 2017-10-20 to 2017-10-22 in Sydney.')
